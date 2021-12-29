6172a42189a460b0a1f15f72514c7585df4b212a
Anastasia M <anastasiamaschenko103@gmail.com>
2021-12-29 10:17:43 +0300

    Rewrite script

--- hopfield.py
+++ hopfield.py
@@ -19,57 +19,63 @@ def bipolarize(img, size, array):
 
 ## SET IMAGE SIZE AND ZERO WEIGHT MATRIX
 
-size = 20
-w = np.zeros((size, size))
-examples = np.array([])
+size = 4
+weight = np.zeros((size*size, size*size))
+names = listdir('./examples')
+examples = np.zeros((len(names), size*size))
 
 ## READ IMAGES INTO BIPOLAR VECTORS
 
-names = listdir('./examples')
+n = 0
 for name in names:
     image = Image.open(f"./examples/{name}")
 
     img = image.load()
-    a = np.array([])
-    a = bipolarize(img, size, a)
-    examples = np.append(examples, a)
+    img_vec = np.array([])
+    img_vec = bipolarize(img, size, img_vec)
+    examples[n] = img_vec
+    n += 1
 
-    ## FILL WEIGHT FOR CURRENT VECTOR
-    for x in range(size):
-        for y in range(size):
-            w[x, y] = w[x, y] + a[x] * a[y]
-
-## NORMALIZE WEIGTH MATRIX
-for x in range(size):
-    w[x, x] = 0
-
-for x in range(size):
-    for y in range(size):
-        w[x, y] = w[x, y] / size
-
-print(w)
+## FILL WEIGHTS
+for x in range(size*size):
+    for y in range(size*size):
+        if x == y:
+            weight[x, y] = 0
+        else:
+            weight[x, y] = np.dot(examples[:,x], examples[:,y]) / size
+            weight[y, x] = weight[x, y]
 
 ## RECOGNIZE TEST IMAGE
-test_image = Image.open("./tests/0.png")
+test_image = Image.open("./tests/1.png")
 img = test_image.load()
-test_vector = bipolarize(img, size, np.array([]))
-current_vector = np.zeros(size)
+test_vect = bipolarize(img, size, np.array([]))
 
-answer = None
-for step in range(1000):
-    for x in range(size):
-        d = 0
-        for y in range(size):
-            d += w[x, y] * test_vector[y]
-            if d > 0:
-                current_vector[x] = 1
-            else:
-                current_vector[x] = -1
+max_diff = size * size * 0.1
 
-    for example in examples:
-        if np.array_equal(current_vector, example):
-            answer = example
-            break
+while True:
+    curr_vect = np.dot(weight, test_vect)
 
-print(answer)
+    for x in range(size*size):
+        if curr_vect[x] > 0:
+            curr_vect[x] = 1
+        else:
+            curr_vect[x] = -1
+
+    distance = 0
+    for x in range(size*size):
+        if curr_vect[x] != test_vect[x]:
+            distance += 1
+
+    print(test_vect)
+    print(curr_vect)
+    if distance > max_diff:
+        test_vect = curr_vect
+    else:
+        break
+
+print(curr_vect)
+
+for x in range(len(examples)):
+    if np.array_equal(curr_vect, examples[x]):
+        print(x)
 
